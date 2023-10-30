6128f5dd2193fee56c762e5c34b81be6f5f99e8c
koka <koka.code@gmail.com>
2023-10-30 18:14:21 +0900

    chore(treesitter): highlight box_patterns

--- after/queries/rust/highlights.scm
+++ after/queries/rust/highlights.scm
@@ -3,6 +3,8 @@
 ((crate) @namespace.crate @nospell)
 
 ;; feature(box_patterns)
+((identifier) @type.qualifier.box
+ (#eq? @type.qualifier.box "box"))
 ((tuple_struct_pattern
    (identifier) @type.qualifier.box)
     (#eq? @type.qualifier.box "box"))
