31acd0e8945988fba427590f8360956c40e8eb22
koka <koka.code@gmail.com>
2025-12-18 15:23:23 +0900

    chore: remove nvim-treesitter-textobjects due to incompatibility

--- lua/plugins/core.lua
+++ lua/plugins/core.lua
@@ -5,7 +5,6 @@ return {
     event = "InsertEnter",
     dependencies = {
       "nvim-treesitter/nvim-treesitter",
-      "nvim-treesitter/nvim-treesitter-textobjects",
     },
     config = function()
       require("nvim-surround").setup()
